c02da2c1e128bde06aa2d61a08002814aa3b7c63
mikeamorim <migamorim2012@gmail.com>
2021-02-19 15:20:15 +0000

    Add a welcoming message

--- HelloWorld/src/Application.java
+++ HelloWorld/src/Application.java
@@ -1,5 +1,5 @@
 public class Application {
-    public static void main(String args[]) {
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Thank you for using the Hello World App");
     }
 }
