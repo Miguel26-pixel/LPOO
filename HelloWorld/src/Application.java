5fd13022144711e4d99a05868aebf1b5a42445f9
mikeamorim <migamorim2012@gmail.com>
2021-02-19 15:21:07 +0000

    Add the version number

--- HelloWorld/src/Application.java
+++ HelloWorld/src/Application.java
@@ -1,5 +1,6 @@
 public class Application {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Version 1.0");
     }
 }
